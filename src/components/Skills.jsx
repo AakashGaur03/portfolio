9445b728d98f37f700924502748863c1574d1e43
Aakash Gaur <aakash.sunrise23@gmail.com>
2025-03-16 00:43:53 +0530

    Update Skills.jsx

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -15,38 +15,43 @@ import { BsLightning } from "react-icons/bs";
 import Heads from "./Heads";
 
 const skillsData = [
-	{
-		category: "Frontend Technologies",
-		skills: "HTML, CSS, JavaScript, NextJS, React, Vue, AJAX, jQuery, Bootstrap, Tailwind",
-	},
-	{
-		category: "Backend Technologies",
-		skills: "NestJS, Node.js, ExpressJS, MongoDB",
-	},
-	{
-		category: "Other Skills",
-		skills: "Git, GitHub, GitLab",
-	},
+  {
+    category: "Frontend Technologies",
+    skills: "HTML, CSS, JavaScript, NextJS, React, Vue, AJAX, jQuery, Bootstrap, Tailwind",
+  },
+  {
+    category: "Backend Technologies",
+    skills: "NestJS, Node.js, ExpressJS, MongoDB",
+  },
+  {
+    category: "Cloud & DevOps",
+    skills:
+      "AWS (Fargate, EC2, VPC, Internet Gateway, NAT Gateway, ALB, Security Groups, Route Tables) CI/CD (GitHub Actions)",
+  },
+  {
+    category: "Other Skills",
+    skills: "Git, GitHub, GitLab",
+  },
 ];
 
 const Skills = () => {
-	return (
-		<div className="px-2">
-			<Heads logo={<BsLightning size={30} />} title={"SKILLS"} />
-			<div className="mt-4 bg-[#1a1a1a] p-5 rounded-lg shadow-lg">
-				<ul className="space-y-3 text-gray-300">
-					{skillsData.map((skill, index) => (
-						<li key={index} className="flex items-start space-x-2">
-							<div className="w-2 h-2 bg-[#47e5c3] rounded-full mt-2"></div>
-							<span>
-								<strong className="text-white">{skill.category}:</strong> {skill.skills}
-							</span>
-						</li>
-					))}
-				</ul>
-			</div>
-		</div>
-	);
+  return (
+    <div className="px-2">
+      <Heads logo={<BsLightning size={30} />} title={"SKILLS"} />
+      <div className="mt-4 bg-[#1a1a1a] p-5 rounded-lg shadow-lg">
+        <ul className="space-y-3 text-gray-300">
+          {skillsData.map((skill, index) => (
+            <li key={index} className="flex items-start space-x-2">
+              <div className="w-2 h-2 bg-[#47e5c3] rounded-full mt-2"></div>
+              <span>
+                <strong className="text-white">{skill.category}:</strong> {skill.skills}
+              </span>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Skills;
